3c4a4402cb34a4c2dc46e06ac6b4fd6c366fd084
CieciaOne <CieciaV2.0@gmail.com>
2024-03-05 18:08:29 +0100

    YKI-01 Introduce adding articles

--- src/articles.jsx
+++ src/articles.jsx
@@ -1,6 +1,7 @@
 import useFetch from "react-fetch-hook";
 import Markdown from "react-markdown";
 import { articlesUrl } from "./common";
+import { useState } from "preact/hooks";
 
 export function Articles() {
     let { data, isLoading, error } = useFetch(articlesUrl)
@@ -9,18 +10,77 @@ export function Articles() {
     return isLoading ? (
         <div>Loading...</div>
     ) : (
-        <div>
-            {data && data.map((d) => <Article title={d.title} body={d.article} />)}
+        <div class="articles">
+            {data && data.map((d) => <Article id={d.id} title={d.title} body={d.article} />)}
+            <AddArticleForm />
         </div>
     );
+
 }
 
 function Article(props) {
     return (
-        <div>
+        <div class="article">
             <hr></hr>
             <h2>{props.title}</h2>
             <Markdown>{props.body}</Markdown>
         </div>
+    );
+}
+
+function AddArticleForm(props) {
+    const [postData, setPostData] = useState({
+        title: '',
+        article: ''
+    });
+
+    const handleClick = () => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(postData)
+        };
+
+        fetch(articlesUrl, requestOptions)
+            .then(_response => {
+                window.location.reload();
+            })
+            .catch(error => {
+                // Handle errors
+                console.error('Error:', error);
+            });
+    };
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setPostData(prevData => ({
+            ...prevData,
+            [name]: value
+        }));
+    };
+
+    return (
+        <div>
+            <hr></hr>
+            <input
+                class="articleTitle"
+                width="100%"
+                name="title"
+                type="text"
+                value={postData.title}
+                onChange={handleInputChange}
+                placeholder="Enter title"></input>
+            <br />
+            <textarea
+                class="articleContent"
+                width="100%"
+                rows={4}
+                cols={60}
+                name="article"
+                type="text"
+                value={postData.article}
+                onChange={handleInputChange}
+                placeholder="Input article content..."></textarea>
+            <button class="addArticleButton" onClick={handleClick}>Add article</button>
+        </div >
     );
 }
